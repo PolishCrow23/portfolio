0f2cfed91f4f1dc98050719e3d10d88038e22858
unknown <igaspodzieja00@gmail.com>
2024-06-05 12:22:42 +0200

    added footer color

--- .netlify/functions-internal/ssr/chunks/_slug__DwfTtW_A.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/_slug__Bvd_-bT4.mjs').then(n => n._);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_CSWlruK5.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_bfvMXr2e.mjs');
-
-export { page };
